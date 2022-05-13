25d43090f6ecb9c0a0e6205e58d30c648967e132
jsueiro <juan.sueiro@outlook.com>
2022-05-13 12:57:45 +0200

    add initial methods

--- test-api/app/controllers/api/v1/products_controller.rb
+++ test-api/app/controllers/api/v1/products_controller.rb
@@ -1,7 +1,50 @@
 class Api::V1::ProductsController < ApplicationController
+  
+  # index renders all items in the products table
   def index
+    products = Product.all
+    render json: products, status: 200
   end
 
-  def show
+  # create, lets us create a new product
+  # if the product is saved -> render json data of the product
+  # if the product is not saved -> render an err obj
+  def create
+  
+    product = Product.new(
+      name: prod_params[:name],
+      brand: prod_params[:brand],
+      price: prod_params[:price],
+      description: prod_params[:description]
+    )
+
+    if product.save
+      render json: product, status: 200
+    else
+      render json: {error: "Error creating product."}
+    end
+
   end
+
+  # the show method searches the product by id. if found, renders json obj
+  # otherwise renders err obj
+  def show
+    product = Product.find_by(id: params[:id])
+    if product:
+      render json: product, status: 200
+    else
+      render json: {error: "Product not found."}
+    end
+  end
+
+  # private method only available to this controller.
+  # uses the built-in methods .require and .permit provided by ActionController for added security
+  private
+  def prod_params
+    params.require(:product).permit([
+      :name,
+      :brand,
+      :price,
+      :description
+    ])
 end
